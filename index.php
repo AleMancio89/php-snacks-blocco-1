7716419d5a51f9332319af9d5f55e557677b9ba9
Alessandro <alessandro.mancinelli89@gmail.com>
2020-12-22 20:50:24 +0100

    #2 snack done

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
-/* 
+/*
+PHP Snack 1 - PHP Loops
+
 Creiamo un array 'matches' contenente altri array i quali rappresentano delle partite di basket di un’ipotetica tappa del calendario. Ogni array della partita avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
 Stampiamo a schermo tutte le partite con questo schema:
 Olimpia Milano - Cantù | 55-60
@@ -36,6 +38,32 @@ for ($i = 0; $i < count($matches); $i++) {
     $keys = array_keys($matches);
     $match = $keys[$i];
     echo $matches[$match]['Home'] . ' - ' . $matches[$match]['Away'] . ' | ' . $matches[$match]['Score Home'] . '-' . $matches[$match]['Score Away'] . '<br>' ;
+}
+
+
+?>
+
+<?php echo "<br> <br> <br>";?>
+
+<?php 
+/* 
+PHP Snack 2
+Passare come parametri GET name, mail e age everificare (cercando i metodi che non conosciamo nella documentazione) che:
+1. name sia più lungo di 3 caratteri,
+2. che mail contenga un punto e una chiocciola
+3. e che age sia un numero.
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+
+*/
+
+$name = $_GET['name'];
+$mail = $_GET['mail'];
+$age = $_GET['age'];
+
+if((strlen($name) > 3) && (strpos($mail, '@') !== false) && (strpos($mail, '.') !== false) && (is_numeric($age))) {
+    echo 'Accesso consentito';
+} else {
+    echo 'Accesso NON consentito';
 }
 
 ?>
